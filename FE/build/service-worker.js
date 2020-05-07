8ebbd063dd62066d7babd0578aa2700579bb14d5
marcellinuskristanto <marcellinus_kristanto@hotmail.com>
2020-05-07 22:12:22 +0700

    Revisi 2 - BUILD

--- FE/build/service-worker.js
+++ FE/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3f6f7e6219c6cfae85cf98f2197a5b0.js"
+  "./precache-manifest.78cd20c2ce5970447d84a828e48173be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
