8e0934c7476e22367ca144f4791cf9f47cc270ea
VIctor Boy Simamora <quedeve@gmail.com>
2020-07-27 18:49:13 +0700

    Update db.config.js
    Update db.Config

--- BE/app/config/db.config.js
+++ BE/app/config/db.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    // CONSTRING: process.env.CONSTRING
-    CONSTRING: 'mongodb://localhost:27017/TestCRM'
+    CONSTRING: process.env.CONSTRING
+    // CONSTRING: 'mongodb://localhost:27017/TestCRM'
 };
